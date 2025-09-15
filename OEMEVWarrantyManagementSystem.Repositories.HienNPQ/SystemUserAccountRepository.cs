b3ba77af86323f0bba6444b8c9620c03d627f159
Hien <hiennpqse180072@fpt.edu.vn>
2025-09-15 12:39:56 +0700

    Add project files.

---
+++ OEMEVWarrantyManagementSystem.Repositories.HienNPQ/SystemUserAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OEMEVWarrantyManagementSystem.Repositories.HienNPQ.Basic;
+using OEMEVWarrantyManagementSystem.Repositories.HienNPQ.DBContext;
+using OEMEVWarrantyManagementSystem.Repositories.HienNPQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEMEVWarrantyManagementSystem.Repositories.HienNPQ
+{
+    public class SystemUserAccountRepository : GenericRepository<SystemUserAccount>
+    {
+        public SystemUserAccountRepository() { }
+        public SystemUserAccountRepository(FA25_PRN232_SE1713_G5_OEMEVWarrantyManagementSystemContext context) => _context =context;
+        public async Task<SystemUserAccount> GetUserAccount(string username, string password)
+        {
+            return await _context.SystemUserAccounts.FirstOrDefaultAsync(u => u.UserName == username && u.Password == password && u.IsActive == true);
+        }
+
+    }
+}
