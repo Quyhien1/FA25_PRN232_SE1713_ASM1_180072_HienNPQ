0d57596b44c5d15a818f21a5e8bddb46b4f35051
Hien <hiennpqse180072@fpt.edu.vn>
2025-09-18 10:31:17 +0700

    15/9/2025

---
+++ OEMEVWarrantyManagementSystem.Service.HienNPQ/SystemUserAccountService.cs
@@ -0,0 +1,27 @@
+﻿using OEMEVWarrantyManagementSystem.Repositories.HienNPQ;
+using OEMEVWarrantyManagementSystem.Repositories.HienNPQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEMEVWarrantyManagementSystem.Service.HienNPQ
+{
+    public class SystemUserAccountService
+    {
+        private readonly SystemUserAccountRepository _repository;
+        public SystemUserAccountService() => _repository = new SystemUserAccountRepository();
+        public async Task<SystemUserAccount> GetUserAccount(string username, string password)
+        {
+            try
+            {
+                return await _repository.GetUserAccount(username, password);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
